6e2b9a760870383f056566663093eb3ce15d571b
Chad Engler <englercj@live.com>
2016-08-31 18:01:42 -0700

    move anchor to TextureComponent; makes more sense there

--- plugins/textures/src/TextureComponent.js
+++ plugins/textures/src/TextureComponent.js
@@ -19,6 +19,30 @@ export default function TextureComponent(Base)
              * @member {Texture}
              */
             this._texture = null;
+
+            /**
+             * The X coord of the attachment point of the texture.
+             *
+             * @private
+             * @member {number}
+             */
+            this._anchorX = 0;
+
+            /**
+             * The Y coord of the attachment point of the texture.
+             *
+             * @private
+             * @member {number}
+             */
+            this._anchorY = 0;
+
+            /**
+             * Tracker for if the vertices are dirty.
+             *
+             * @private
+             * @member {number}
+             */
+            this._anchorDirty = true;
         }
 
         /**
@@ -41,6 +65,48 @@ export default function TextureComponent(Base)
             this._texture = v;
 
             this._onTextureChange();
+        }
+
+        /**
+         * The X coord of the attachment point of the texture.
+         *
+         * @member {number}
+         */
+        get anchorX()
+        {
+            return this._anchorX;
+        }
+
+        /**
+         * @param {number} v - The new anchor.
+         */
+        set anchorX(v)
+        {
+            if (this._anchorX === v) return;
+
+            this._anchorX = v;
+            this._anchorDirty = true;
+        }
+
+        /**
+         * The Y coord of the attachment point of the texture.
+         *
+         * @member {number}
+         */
+        get anchorY()
+        {
+            return this._anchorY;
+        }
+
+        /**
+         * @param {number} v - The new anchor.
+         */
+        set anchorY(v)
+        {
+            if (this._anchorY === v) return;
+
+            this._anchorY = v;
+            this._anchorDirty = true;
         }
 
         /**
